93b4ac1c6dab9629f3e3af38fb7c38e479e3b58e
Hiraku NAKANO <hiraku@tojiru.net>
2014-05-02 11:47:08 +0900

    add docComment

--- src/View.php
+++ src/View.php
@@ -7,16 +7,24 @@ namespace Spindle;
 
 use ArrayObject;
 
+/**
+ * 素のPHPテンプレートにlayout機能を付加するシンプルなレンダラーです
+ */
 class View implements \IteratorAggregate
 {
     protected
-        $_storage
-    ,   $_basePath
-    ,   $_fileName
-    ,   $_layoutFileName = ''
-    ,   $_content = ''
+        /** @var ArrayObject */ $_storage
+    ,   /** @var string */ $_basePath
+    ,   /** @var string */ $_fileName
+    ,   /** @var string */ $_layoutFileName = ''
+    ,   /** @var string */ $_content = ''
     ;
 
+    /**
+     * @param string $fileName 描画したいテンプレートのファイル名を指定します
+     * @param string $basePath テンプレートの探索基準パスです。相対パスも指定できます。指定しなければinclude_pathから探索します。
+     * @param ArrayObject $arr
+     */
     function __construct($fileName, $basePath='', ArrayObject $arr=null)
     {
         $this->_storage = $arr ?: new ArrayObject(array(), ArrayObject::ARRAY_AS_PROPS);
@@ -24,40 +32,68 @@ class View implements \IteratorAggregate
         $this->_basePath = rtrim($basePath, \DIRECTORY_SEPARATOR);
     }
 
+    /**
+     * このクラスはforeach可能です
+     * @return \ArrayIterator
+     */
     function getIterator()
     {
         return $this->_storage->getIterator();
     }
 
+    /**
+     * @param string|int $name
+     * @return mixed
+     */
     function __get($name)
     {
         return $this->_storage[$name];
     }
 
+    /**
+     * @param string|int $name
+     * @param mixed $value
+     */
     function __set($name, $value)
     {
         $this->_storage[$name] = $value;
     }
 
+    /**
+     * @param string|int $name
+     * @return bool
+     */
     function __isset($name)
     {
         return isset($this->_storage[$name]);
     }
 
+    /**
+     * 描画するスクリプトファイルのパスを返します。
+     * @return string
+     */
     function __toString()
     {
         if ($this->_basePath) {
             return $this->_basePath . \DIRECTORY_SEPARATOR . $this->_fileName;
         } else {
-            return $this->_fileName;
+            return (string)$this->_fileName;
         }
     }
 
+    /**
+     * セットされたview 変数を配列化して返します
+     * @return array
+     */
     function toArray()
     {
         return (array)$this->_storage;
     }
 
+    /**
+     * 配列で一気にview変数をセットします
+     * @param array|\Traversable $array
+     */
     function assign($array)
     {
         if (!is_array($array) && !($array instanceof \Traversable)) {
@@ -69,6 +105,10 @@ class View implements \IteratorAggregate
         }
     }
 
+    /**
+     * @param string $name
+     * @param array $array
+     */
     function append($name, $array)
     {
         $s = $this->_storage;
@@ -89,6 +129,10 @@ class View implements \IteratorAggregate
         }
     }
 
+    /**
+     * テンプレートファイルを描画して文字列にして返します
+     * @return string
+     */
     function render()
     {
         foreach ($this->_storage as ${"\x00key"} => ${"\x00val"}) {
@@ -111,26 +155,48 @@ class View implements \IteratorAggregate
         }
     }
 
+    /**
+     * @internal
+     */
     protected function setContent($content)
     {
         $this->_content = $content;
     }
 
+    /**
+     * 子テンプレートの描画結果を文字列として返します
+     * @return string
+     */
     function content()
     {
         return $this->_content;
     }
 
+    /**
+     * 親となるレイアウトテンプレートのファイル名を指定します。
+     * レイアウトは__construct()で指定した基準パスと同じパスから探索します。
+     * @param string $layoutFileName
+     */
     function setLayout($layoutFileName)
     {
         $this->_layoutFileName = $layoutFileName;
     }
 
+    /**
+     * 現在セットされているレイアウトファイル名を返します
+     * @return string
+     */
     function getLayout()
     {
         return $this->_layoutFileName;
     }
 
+    /**
+     * 指定したテンプレートファイルを描画します。
+     * 変数は引き継がれます。
+     * @param string $partialFileName
+     * @return string
+     */
     function partial($partialFileName)
     {
         $partial = new static(
